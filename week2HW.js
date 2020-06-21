7a8879b4fad7e6a9e601e1fef28dfa0bfa596b8d
Israel Belete <ibelete2000@gmail.com>
2020-06-20 20:22:04 -0600

    added example

--- week2HW.js
+++ week2HW.js
@@ -1,13 +1,21 @@
 // week 2 Home Work:
 /* HomeWork:
     1. Declare variable with string. ex. var name = ""
-    2. Print how much coffee will be consume tonight.
+    2. Print how much drink will be consume tonight.
         ex . console.log(`Today, I consume ${ name[]} cups of ${ name[]})`);
+        ex2. var drinks = ["Coffee", "tea", "milk"];
+            var location = ["library", "home"];
+
+console.log(`Today, I consume ${drinks[1]} in the morning, then I went to the ${location[1]}`);
     3. Declare array and use .length() method to find out how many are there?
     4. Con't from Q3 - use push in array
     5. Create new array and use reverse()method and join()
    BONUS:
     6. Use any High methods order: .forEach, .fiter(), .reduce(). create arrays.
 */
-/***** Answer Question belew here **** */
+/***** Answer Question below here *****/
 
+var drinks = ["Coffee", "tea", "milk"];
+var location = ["library", "home"];
+
+console.log(`Today, I consume ${drinks[1]} in the morning, then I went to the ${location[1]}`);
